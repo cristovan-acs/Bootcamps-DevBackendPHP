1a555713fc00534ffdf86b2f5186cb2d2ce9c60c
cristovan-acs <cristovan.acs@gmail.com>
2020-06-27 17:24:42 -0300

    Adicionando validação de campos no formulario

--- aula_php/script.php
+++ aula_php/script.php
@@ -1,6 +1,6 @@
 <?php
 
-//$categoria = []; **** Não funciona
+//$categoria = []; **** Não funciona com meu navegador
 $categoria;  
 $categoria[] = 'infantil';
 $categoria[] = 'adolescente';
@@ -10,17 +10,41 @@ $categoria[] = 'idoso';
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
-
 //var_dump($nome);
 //var_dump($idade);
 //return 0;
 
-if ($idade >= 6 && $idade <= 12)
+if(empty($nome))
+{
+    echo 'O nome não pode ser vazio!';
+    return;
+}
+
+if(strlen($nome) < 3)
+{
+    echo 'O nome deve conter mais de 3 caracteres!';
+    return;
+}
+
+if(strlen($nome) > 40)
+{
+    echo "O nome e muito extenso!";
+    return;
+}
+
+if(!is_numeric($idade))
+{
+    echo 'Informe um numero para idade!';
+    return;
+}
+
+if ($idade >= 1 && $idade <= 12)
 {
     for($i = 0; $i <= count($categoria); $i++)
     {
         if($categoria[$i] == 'infantil')
-            echo "O nadador ".$nome. " compete na categoria infantil"; 
+            //echo "O nadador ".$nome. " compete na categoria infantil"; 
+            echo "O nadador ".$nome. " compete na categoria ".$categoria[$i];
     }
     //echo 'infantil';
 }
